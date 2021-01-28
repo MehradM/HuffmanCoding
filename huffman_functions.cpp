ef161ca874256f71a953a7cc2f996e9de4d89f53
Mehrad Moshiri <mehradmoshiri09@gmail.com>
2021-01-28 12:32:00 +0330

    add str_to_str01 & str01_to_cmpStr functions

--- huffman_functions.cpp
+++ huffman_functions.cpp
@@ -66,6 +66,22 @@ string file_to_string(const string& fileName) {
 
 std::string huffman_code_string(const string &str) {
     auto pqueue = make_queue(str);
+    auto root = createTree(pqueue).top();
+    auto charMap = character_map(pqueue,root);
+    auto str01 = str_to_str01(str,charMap);
+    auto cmpStr = str01_to_cmpStr(str);
+    string huffmanString;
+    huffmanString += to_string(str.size());
+    huffmanString.push_back('\n');
+    huffmanString += to_string(charMap.size());
+    huffmanString.push_back('\n');
+    for (const auto& el : charMap) {
+        huffmanString.push_back(el.first);
+        huffmanString += " " + el.second.first;
+        huffmanString.push_back('\n');
+    }
+    huffmanString += cmpStr;
+    return cmpStr;
 }
 
 tree_pqueue make_queue(const string &str) {
@@ -82,3 +98,38 @@ tree_pqueue make_queue(const string &str) {
     }
     return pqueue;
 }
+
+std::string str_to_str01(const string &str,const char_map& map) {
+    string str01;
+    for (const auto& el : str) {
+        str01 += map.at(el).first;
+    }
+    unsigned int num = str01.size() % 8;
+    unsigned int need = (num == 0 ? 0 : (8 - num));
+    str01.append(need,'0');
+    return str01;
+}
+
+std::string str01_to_cmpStr(const string &str) {
+    string cmpStr;
+    for (int i = 0; i < str.length(); i+=8) {
+        cmpStr.push_back(str8bit_to_char(str.substr(i,8)));
+    }
+    return cmpStr;
+}
+
+char str8bit_to_char(const string &str8bit) {
+    char a = 0;
+    for (int i = 0; i < 8; ++i) {
+        a <<= 1;
+        if(str8bit[i] == '1'){
+            a |= 1;
+        }
+        else {
+            a &= -2;
+        }
+    }
+    return a;
+}
+
+
--- huffman_functions.h
+++ huffman_functions.h
@@ -23,10 +23,14 @@ public:
     }
 };
 typedef std::priority_queue<TreeNode*, std::vector<TreeNode*>, compare> tree_pqueue;
+typedef std::map<char, std::pair<std::string,int>> char_map;
 tree_pqueue createTree(tree_pqueue);
 std::map<char, std::pair<std::string,int>> character_map(tree_pqueue pqueue,TreeNode * root);
 void make_compressed_file(const std::string& fileName);
 std::string file_to_string(const std::string& fileName);
 std::string huffman_code_string(const std::string& str);
 tree_pqueue make_queue(const std::string& str);
+std::string str_to_str01(const std::string& str,const char_map& map);
+std::string str01_to_cmpStr(const std::string& str);
+char str8bit_to_char(const std::string& str8bit);
 #endif //HUFFMANPROJECT_HUFFMAN_FUNCTIONS_H
